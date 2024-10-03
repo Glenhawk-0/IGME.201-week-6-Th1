6a627bf95766b0d6d675ec6e48bc86a7f9fe8186
Glenhawk-0 <LennonEhare@Gmail.com>
2024-10-03 08:21:53 -0400

    added string player name and intro line
    the jungleeeee

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //setup 
+            string playerName = "adventurer";
+
+            Console.Write("Welcome to the jungle \nWe got fun and games \nWe got everything you want \nHoney, we know the names \n ");
+
+        }
+    }
+}
