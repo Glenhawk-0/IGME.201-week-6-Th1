4552c0cf9eba3995d463301e912af334ad8852fa
Glenhawk-0 <LennonEhare@Gmail.com>
2024-10-03 08:31:38 -0400

    jeegus. quite alot there nick. surely theirs a better way >:]

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -38,6 +38,44 @@
             userChoice = Console.ReadLine();
             Console.WriteLine("\nYou chose: " + userChoice + "\n");
 
+            if (userChoice == "left")
+            {
+                //Go to the left
+
+            }
+            else if (userChoice == "right")
+            {
+                //Go to the right
+            }
+            else
+            {
+                //Oh no! the user typed in something else.
+                userChoice = "NONE";
+                while (userChoice != "left" && userChoice != "right" && userChoice != "up")
+                {
+                    Console.WriteLine("Please enter either 'left', 'right', or our now super    secret answer of 'up'.");
+                    userChoice = Console.ReadLine();
+                    Console.WriteLine("\nYou chose: " + userChoice + "\n");
+                }
+                if (userChoice == "left")
+                {
+                    //Go to the left
+                }
+                else if (userChoice == "right")
+                {
+                    //Go to the right
+                }
+                else if (userChoice == "up")
+                {
+                    //Easter Egg
+                    Console.WriteLine("'up' seriously? 'up'!? How can we go up? UP YOURS! |_(O_O)_/ **Flips keyboard** CLOSE THE PROGRAM!");
+                }
+                else
+                {
+                    Console.WriteLine("I honestly don't know how you got here. Congrats.");
+                }
+            }
+
         }//end static void
     }//end internal class
 }//end
