3d903a09c9486e6ef56aac4eb1117474361feea9
Glenhawk-0 <LennonEhare@Gmail.com>
2024-10-03 08:27:31 -0400

    i uh... added alot ...

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -5,11 +5,35 @@
         static void Main(string[] args)
         {
 
-            //setup 
+            //varibles
             string playerName = "adventurer";
+            string userChoice = "NONE";
 
+
+
+            //--
+
+            //intro to jungle
             Console.Write("Welcome to the jungle \nWe got fun and games \nWe got everything you want \nHoney, we know the names \n ");
+            Console.WriteLine("Wait... I don't actually know your name.");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("[Please enter your name, then press 'ENTER' on the keyboard]");
 
-        }
-    }
-}
+            //added user input
+            playerName = Console.ReadLine();
+            Console.WriteLine("\nWelcome to the jungle " + playerName + "!");
+
+            // start adventurin the jungle
+            Console.WriteLine("" +
+            "We approach a clearing in the jungle\n" +
+            "There seems to be a trail to the left...\n" +
+            "And a cave entrance to the right.\n"
+                );
+
+            //present choice
+            Console.WriteLine("Which path do you chose?");
+            Console.WriteLine("Type 'left' to choose the path and go left or type 'right' to choose the cave and go right.");
+
+        }//end static void
+    }//end internal class
+}//end
